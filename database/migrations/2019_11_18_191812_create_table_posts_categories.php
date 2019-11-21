b49a196470b12d53d871115b03b1e39058d235c9
prmelo-msn <prmelo-msn@hotmail.com>
2019-11-21 12:58:32 -0300

    comentario delete cascade

--- database/migrations/2019_11_18_191812_create_table_posts_categories.php
+++ database/migrations/2019_11_18_191812_create_table_posts_categories.php
@@ -18,7 +18,7 @@ class CreateTablePostsCategories extends Migration
          Schema::create('posts_categories', function (Blueprint $table) {
              $table->unsignedBigInteger('post_id');
              $table->unsignedBigInteger('category_id');
-            
+             // Esse código fez com que fosse possível realizar o delete cascade!! - Ajuda Arthur
              $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
              $table->foreign('category_id')->references('id')->on('categories');
              });
